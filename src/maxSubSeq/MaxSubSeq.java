6446a6f51ea5a4899ed18e493dbc4b7976768b17
chenlinwu <chenlinwu1124@gmail.com>
2014-01-21 18:36:37 -0600

    2nd edition

--- src/maxSubSeq/MaxSubSeq.java
+++ src/maxSubSeq/MaxSubSeq.java
@@ -5,60 +5,58 @@ import java.util.List;
 
 
 public class MaxSubSeq {
-	
+
 	static List<Integer> subSeq=new ArrayList<Integer>();
 
-	public static int findMaxSubSeq(int[] seq) {
-		int a=0;
-		int b=0;
-		int sum=seq[0];
-		boolean re=false;
-		for (int i = 0; i < seq.length; i++) {
+	
+	static int MaxSubsequenceSum(int A[], int N)
+	{
+		int ThisSum, MaxSum;
 		
-			if(a<0){
-				a=seq[i];
-				b=i;
-				re=true;
-			}
-			else {
-				a+=seq[i];
-				
-			}
+		ThisSum = 0;
+		MaxSum=A[0];
+		/*for recording the subseq*/
+		int j=0;
+		boolean re=false;
 
-			if(a>sum){
-				sum=a;
-				for(int j=b;j<=i;j++){
+		for (int i = 0; i < N; i++)
+		{
+			ThisSum += A[i];
+			if (ThisSum > MaxSum){
+				MaxSum = ThisSum;
+				/*for recording the subseq	*/
+				for(int k=j;k<=i;k++){
 					if(re==true) {
 						subSeq.clear();
 						re=false;
 					}
-				    subSeq.add(seq[j]);
+					subSeq.add(A[k]);
 				}
-				b=i+1;
+				j=i+1;
+				
 			}
-		}
-		if (sum<=0) {
-			for (int i = 0; i < seq.length; i++) {
-				if(seq[i]>=sum) {
-					sum=seq[i];
-				subSeq.clear();
-				subSeq.add(seq[i]);
-				}
+			else if (ThisSum < 0){
+				ThisSum = 0;
+				re=true;
+				j=i+1;
 			}
+
 		}
-		return sum;
-		
+		return MaxSum;
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] s= {-20, -2,- 3,- 10, -4, -7,-2, -5
-				};
-		int maxSum=findMaxSubSeq(s);
-		System.out.println(subSeq+"\n"+maxSum);// TODO Auto-generated method stub
+		int[] s= {-20, -2,- 3,10, -4, 7,2, -5
+		};
+		for (int i = 0; i < s.length; i++) {
+			System.out.print(s[i]+" ");
 
+		}
+		System.out.println();
+		System.out.println(MaxSubsequenceSum(s, s.length)+"\t"+subSeq);
 	}
 
 }
